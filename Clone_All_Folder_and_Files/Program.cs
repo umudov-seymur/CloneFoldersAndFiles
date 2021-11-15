42581989cd2c1359ca0da1e51141f229b6dcdb7e
Seymur Umudov <u.seymur@outlook.com>
2021-11-15 13:57:00 +0400

    Initial commit

---
+++ Clone_All_Folder_and_Files/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Clone_All_Folder_and_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = Environment.CurrentDirectory;
+            string destPath = Path.Combine(Environment.CurrentDirectory, "ClonedFolder");
+
+            CopyFolder(sourcePath, destPath);
+        }
+
+        /// <summary>
+        /// Function to recursively clone a all folder and files to another folder
+        /// </summary>
+        /// <param name="sourceFolder">Source folder path</param>
+        /// <param name="destFolder">Destination folder path</param>
+        public static void CopyFolder(string sourceFolder, string destFolder)
+        {
+            DirectoryInfo sourceDir = new DirectoryInfo(sourceFolder);
+
+            if (!sourceDir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    $"Source directory does not exist or could be found {sourceFolder}"
+                );
+            }
+
+            DirectoryInfo[] dirs = sourceDir.GetDirectories();
+            if (!Directory.Exists(destFolder))
+            {
+                Directory.CreateDirectory(destFolder);
+            }
+
+            FileInfo[] files = sourceDir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destFolder, file.Name);
+                file.CopyTo(tempPath, true);
+            }
+
+            foreach (DirectoryInfo subDirectory in dirs)
+            {
+                string tempSubDirPath = Path.Combine(destFolder, subDirectory.Name);
+                CopyFolder(subDirectory.FullName, tempSubDirPath);
+            }
+        }
+    }
+}
